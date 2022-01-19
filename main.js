cac10d33c5bce978da676478f8708837f826d457
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-01-19 14:43:07 +0100

    completamento primo progetto js

---
+++ main.js
@@ -0,0 +1,6 @@
+let name = prompt('Qual è il tuo nome?');
+let surname = prompt('Qual è il tuo cognome?');
+let color = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('pwd').innerHTML="La tua password è " + name + surname + color + "21";
+
